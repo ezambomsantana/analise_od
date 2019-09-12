c8872028363984491b15b2821518d0c4b7e702e3
Eduardo Santana <ezambomsantana@gmail.com>
2019-09-12 17:26:31 -0300

    calculate-renda

--- src/shape.py
+++ src/shape.py
@@ -46,18 +46,18 @@ data_mp2 = data17[['NOME_O',  'MP']].groupby(['NOME_O']).sum().sort_values(by=['
 data_mp2 = data_mp2.set_index('NOME_O')
 
 data_mp = data17[['NOME_O', 'MUNI_O', 'FE_VIA']].groupby(['NOME_O','MUNI_O']).sum().sort_values(by=['NOME_O']).reset_index()
-
-can['NomeDistri'] = can['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
 data_mp = data_mp.set_index('NOME_O')
-df = can.set_index('NomeDistri').join(data_mp).join(data_mp2)
+can['NomeDistri'] = can['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
 
+data_renda = data17[['NOME_O', 'RENDA_FA']].groupby(['NOME_O']).mean().sort_values(by=['RENDA_FA']).reset_index()
+data_renda = data_renda.set_index('NOME_O')
 
+df = can.set_index('NomeDistri').join(data_mp).join(data_mp2).join(data_renda)
+print(df)
 df['MEDIA'] = df['MP'] / df['FE_VIA']
 
 df = df[df['MUNI_O'] == 36]
 ### many data 
-fig, ax = plt.subplots(1, 1)
 
-df.plot(column='MEDIA', ax=ax, legend=True)
+plt.scatter(df.MEDIA, df.RENDA_FA)
 plt.show()
-
