3bb5be4634bf9631b05360bf2e2654a11daef231
Eduardo Santana <ezambomsantana@gmail.com>
2019-09-12 22:05:10 -0300

    shape

--- src/shape.py
+++ src/shape.py
@@ -14,7 +14,7 @@ def calculate_weighted_mean(data):
 
 folder_data = "/home/eduardo/dev/analise_od/data/"
 arq17 = "dados17.csv"
-# read the shapefile as a GeoDataFrame
+
 can = gpd.GeoDataFrame.from_file("/home/eduardo/Distritos_2017_region.shp", encoding='latin-1')
 
 data17 = pd.read_csv(folder_data + arq17, dtype={'ZONA_O': str, 'ZONA_D': str}, header=0,delimiter=";", low_memory=False) 
@@ -31,15 +31,14 @@ data17['NOME_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else mydi
 data17 = calculate_weighted_mean(data17)
 
 modos17 = {0:'outros',1:'metro',2:'trem',3:'metro',4:'onibus',5:'onibus',6:'onibus',7:'fretado', 8:'escolar',9:'carro-dirigindo', 10: 'carro-passageiro', 11:'taxi', 12:'taxi-nao-convencional', 13:'moto', 14:'moto-passageiro', 15:'bicicleta', 16:'pe', 17: 'outros'}
-    
-
+  
 coletivo = ['onibus','trem','metro']
 privado = ['carro-dirigindo','moto','bicicleta','taxi']
 
 data17['MODOPRIN'] = data17['MODOPRIN'].replace(modos17)
 data17 = data17[data17['MODOPRIN'].isin(coletivo + privado)] 
 
-data17 = data17[data17['H_SAIDA'] >= 5 ]
+data17 = data17[data17['H_SAIDA'] >= 5]
 data17 = data17[data17['H_SAIDA'] <= 10]
 
 data_mp2 = data17[['NOME_O',  'MP']].groupby(['NOME_O']).sum().sort_values(by=['MP']).reset_index()
@@ -53,11 +52,31 @@ data_renda = data17[['NOME_O', 'RENDA_FA']].groupby(['NOME_O']).mean().sort_valu
 data_renda = data_renda.set_index('NOME_O')
 
 df = can.set_index('NomeDistri').join(data_mp).join(data_mp2).join(data_renda)
-print(df)
 df['MEDIA'] = df['MP'] / df['FE_VIA']
 
 df = df[df['MUNI_O'] == 36]
 ### many data 
 
 plt.scatter(df.MEDIA, df.RENDA_FA)
+plt.show()
+
+
+data17_coletivo = data17[data17['MODOPRIN'].isin(coletivo)] 
+data17_privado = data17[data17['MODOPRIN'].isin(privado)] 
+
+data17_coletivo = data17_coletivo[['NOME_O', 'FE_VIA']].groupby(['NOME_O']).mean().sort_values(by=['FE_VIA']).reset_index()
+data17_coletivo = data17_coletivo.set_index('NOME_O')
+
+data17_privado = data17_privado[['NOME_O', 'FE_VIA']].groupby(['NOME_O']).mean().sort_values(by=['FE_VIA']).reset_index()
+data17_privado = data17_privado.set_index('NOME_O')
+
+df = can.set_index('NomeDistri').join(data17_coletivo).join(data17_privado,lsuffix='_coletivo', rsuffix='_privado').join(data_mp)
+df['por_coletivo'] = df['FE_VIA_coletivo'] / (df['FE_VIA_coletivo'] + df['FE_VIA_privado'])
+df['por_privado'] = df['FE_VIA_privado'] / (df['FE_VIA_coletivo'] + df['FE_VIA_privado'])
+df = df[df['MUNI_O'] == 36]
+
+
+fig, ax = plt.subplots(1, 1)
+
+df.plot(column='por_coletivo', ax=ax, legend=True)
 plt.show()
