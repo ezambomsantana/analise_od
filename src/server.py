bac9bb57635034037eb16fddf1943cb00bee680b
Eduardo Santana <ezambomsantana@gmail.com>
2019-10-12 10:33:55 -0300

    server

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import json
-from server_functions import load_districts, load_subway, load_data17, load_cptm, load_graph, load_curitiba 
+from server_functions import load_districts, load_subway, load_data17, load_cptm, load_graph, load_curitiba, load_zonas
 
 app = Flask(__name__)
 api = Api(app)
@@ -55,7 +55,19 @@ class Graph(Resource):
 
 class Curitiba(Resource):
     def get(self):
-        return load_curitiba().to_json()        
+        return load_curitiba().to_json()  
+
+class Zonas(Resource):
+    def get(self):
+        args = request.args
+        vehicle_type = args['vehicleType']
+        sexo = args['sexo']
+        horarioInicio = args['horarioInicio']
+        horarioFim = args['horarioFim']
+        origin = args['origin']
+        orde = args['orde']
+        mapa = load_zonas(vehicle_type, sexo, horarioInicio, horarioFim, origin, 'ZONA_O')
+        return mapa.to_json()      
 
 api.add_resource(Metro, '/metro')
 api.add_resource(Distritos, '/distritos') 
@@ -63,6 +75,7 @@ api.add_resource(Pontos, '/pontos')
 api.add_resource(CPTM, '/cptm')
 api.add_resource(Graph, '/grafo')
 api.add_resource(Curitiba, '/curitiba')
+api.add_resource(Zonas, '/zonas')
 
 
 if __name__ == '__main__':
--- src/server_functions.py
+++ src/server_functions.py
@@ -21,13 +21,16 @@ mapa = gpd.GeoDataFrame.from_file("../data/shapes/Distritos_2017_region.shp", en
 mapa = mapa.to_crs({"init": "epsg:4326"})
 mapa['NomeDistri'] = mapa['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
 
+zonas = gpd.GeoDataFrame.from_file("../data/shapes/Zonas_2017_region.shp", encoding='latin-1')
+zonas = zonas.to_crs({"init": "epsg:4326"}) 
+zonas['NomeDistri'] = zonas['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
+zonas['NomeZona'] = zonas['NomeZona'].apply(lambda x: unidecode.unidecode(x))
+
 folder_data = "../data/"
 arq17 = "dados17_distance.csv"
 
 data17 = pd.read_csv(folder_data + arq17, dtype={'ZONA_O': str, 'ZONA_D': str}, header=0,delimiter=",", low_memory=False) 
 data17 = data17.dropna(subset=['CO_O_X'])
-
-print(data17)
 
 csv_file = folder_data + "regioes17.csv"
 mydict = []
@@ -35,8 +38,21 @@ with open(csv_file, mode='r') as infile:
     reader = csv.reader(infile, delimiter=";")
     mydict = {rows[0]:rows[1] for rows in reader}
 
+csv_file = folder_data + "zonas17.csv"
+zonas_nomes = []
+with open(csv_file, mode='r') as infile:
+    reader = csv.reader(infile, delimiter=";")
+    zonas_nomes = {rows[0]:rows[1] for rows in reader}
+
 data17['NOME_O'] = data17['ZONA_O'].apply(lambda x: '' if pd.isnull(x) else mydict[x])
 data17['NOME_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else mydict[x])
+
+
+data17['ZONA_O'] = data17['ZONA_O'].apply(lambda x: '' if pd.isnull(x) else zonas_nomes[x])
+data17['ZONA_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else zonas_nomes[x])
+
+print(data17['ZONA_O'])
+
 data17['NUM_TRANS'] = data17[['MODO1', 'MODO2','MODO3','MODO4']].count(axis=1)
 
 data17 = calculate_weighted_mean(data17)
@@ -139,5 +155,47 @@ def load_curitiba():
     return curitiba
 
 
-#mapa = gpd.GeoDataFrame.from_file("../data/shapes/Zonas_2017_region.shp", encoding='latin-1')
-#print(mapa)
+def load_zonas(vehicle, sexo, horarioInicio, horarioFim, origin, orde):
+
+    data17_copy = data17
+
+    if vehicle != "0":
+        vehicles = vehicle.split(",")
+        vehicles_int = []
+        for v in vehicles:
+            vehicles_int.append(int(v))
+        data17_copy = data17_copy[data17_copy['MODOPRIN'].isin(vehicles_int)] 
+
+    if sexo != "0":
+        data17_copy = data17_copy[data17_copy['SEXO'].isin([int(sexo)])]
+
+    if horarioInicio != "0":
+        data17_copy = data17_copy[data17_copy['H_SAIDA'] >= int(horarioInicio)]
+
+    if horarioFim != "0":
+        data17_copy = data17_copy[data17_copy['H_SAIDA'] <= int(horarioFim)]
+
+    if origin != "0":
+        data17_copy = data17_copy[data17_copy['NOME_O'] == origin]
+    
+
+    data_mp2 = data17_copy[[orde,  'MP']].groupby([orde]).sum().sort_values(by=['MP']).reset_index()
+    data_mp2 = data_mp2.set_index(orde)
+
+    data_mp_dist = data17_copy[[orde,  'MP_DIST']].groupby([orde]).sum().sort_values(by=['MP_DIST']).reset_index()
+    data_mp_dist = data_mp_dist.set_index(orde)
+
+    data_mp = data17_copy[[orde, 'MUNI_O', 'FE_VIA']].groupby([orde,'MUNI_O']).sum().sort_values(by=[orde]).reset_index()
+    data_mp = data_mp.set_index(orde)
+
+    data_renda = data17_copy[[orde, 'RENDA_FA']].groupby([orde]).mean().sort_values(by=['RENDA_FA']).reset_index()
+    data_renda = data_renda.set_index(orde)
+
+    data_trans = data17_copy[[orde, 'NUM_TRANS']].groupby([orde]).mean().sort_values(by=['NUM_TRANS']).reset_index()
+    data_trans = data_trans.set_index(orde)
+
+    df = zonas.set_index('NomeZona').join(data_mp).join(data_mp2).join(data_renda).join(data_trans).join(data_mp_dist)
+    df['MEDIA'] = df['MP'] / df['FE_VIA']
+    df['MEDIA_DIST'] = df['MP_DIST'] / df['FE_VIA']
+    df = df.reset_index()
+    return df
