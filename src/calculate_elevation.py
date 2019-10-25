7efd3411bbcfe40a91746913290b4eca9f5531c5
Eduardo Santana <ezambomsantana@gmail.com>
2019-10-25 14:56:46 -0300

    changes

--- src/calculate_elevation.py
+++ src/calculate_elevation.py
@@ -17,7 +17,7 @@ def calculate_distance(origin, dest):
 
 folder_data = "../data/"
 folder_images_maps = "/Users/eduardosantana/pesquisa/analise_od/images/maps/"
-arq17 = "indices.1.csv"
+arq17 = "ind.csv"
 
 data17 = pd.read_csv(arq17, header=0,delimiter=",", low_memory=False) 
 data17.columns = ['id','path']
@@ -26,6 +26,7 @@ count_2 = 0
 count_4 = 0
 count_6 = 0
 
+points = []
 for index, row in data17.iterrows():
     tup = row['path']
     l = literal_eval(tup)
@@ -73,8 +74,10 @@ for index, row in data17.iterrows():
     if bigger_dist > 0:
         if (bigger/bigger_dist)*100 <= 2:
             count_2 = count_2 + 1
+            points.append(l[0][0])
         elif (bigger/bigger_dist)*100 <= 4:
             count_4 = count_4 + 1
+            points.append(l[0][0])
         elif (bigger/bigger_dist)*100 <= 6:
             count_6 = count_6 + 1
         if (bigger/bigger_dist)*100 > 100:
@@ -82,3 +85,19 @@ for index, row in data17.iterrows():
 print(count_2)
 print(count_4)
 print(count_6)
+
+import pandas as pd
+import folium
+
+def generateBaseMap(default_location=[-23.533773, -46.625290], default_zoom_start=12):
+    base_map = folium.Map(location=default_location, control_scale=True, zoom_start=default_zoom_start)
+    return base_map
+
+from folium.plugins import HeatMap
+
+df_copy = pd.DataFrame(points, columns =['lat', 'long', 'alt']) 
+
+df_copy['count'] = 1
+base_map = generateBaseMap()
+m = HeatMap(data=df_copy[['long', 'lat', 'count']].groupby(['long', 'lat']).sum().reset_index().values.tolist(), radius=8, max_zoom=13).add_to(base_map)
+m.save("filename.html")
