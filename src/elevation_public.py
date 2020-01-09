90e5ed7e5f6f3358f53f3c7d8f950840c855c94f
Eduardo Santana <ezambomsantana@gmail.com>
2020-01-09 15:56:04 -0300

    dados

---
+++ src/elevation_public.py
@@ -0,0 +1,83 @@
+import openrouteservice
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import unidecode
+import math
+import seaborn as sns
+import geopy.distance
+import utm
+from shapely.geometry import shape, LineString, Polygon
+
+teste = pd.read_csv('indices_public.csv', index_col=0)
+dict_routes = teste.to_dict('index')
+def calculate_weighted_mean(data):
+    data['FE_VIA'] = data['FE_VIA'].apply(lambda x: 1 if math.isnan(x) else x)
+    data['FE_VIA'] = data['FE_VIA'].apply(lambda x: 1 if int(x) == 0 else x)
+    data['MP'] = data['FE_VIA'] * data['DURACAO']
+    data['MP_DIST'] = data['FE_VIA'] * data['DISTANCE']
+    return data
+
+folder_data = "../data/"
+folder_images_maps = "/Users/eduardosantana/pesquisa/analise_od/images/maps/"
+arq17 = "dados17.csv"
+
+data17 = pd.read_csv(folder_data + arq17, dtype={'ZONA_O': str, 'ZONA_D': str}, header=0,delimiter=";", low_memory=False) 
+data17 = data17.dropna(subset=['CO_O_X'])
+data17['OX'] = data17['CO_O_X'].astype(int)
+data17['OY'] = data17['CO_O_Y'].astype(int)
+data17['DX'] = data17['CO_D_X'].astype(int)
+data17['DY'] = data17['CO_D_Y'].astype(int)
+data17['DISTANCE'] = 0
+
+#filters
+
+data17_sp = data17[data17['MUNI_O'] == 36] # Only SP
+data17_sp = data17[data17['MUNI_D'] == 36] # Only SP
+
+modos17 = {0:'outros',1:'metro',2:'trem',3:'metro',4:'onibus',5:'onibus',6:'onibus',7:'fretado', 8:'escolar',9:'carro-dirigindo', 10: 'carro-passageiro', 11:'taxi', 12:'taxi-nao-convencional', 13:'moto', 14:'moto-passageiro', 15:'bicicleta', 16:'pe', 17: 'outros'}
+data17_sp['MODOPRIN'] = data17_sp['MODOPRIN'].replace(modos17)
+data17_carros = data17_sp[data17_sp['MODOPRIN'].isin(['metro', 'trem', 'onibus'])] 
+
+def calculate_distance(row):  
+    origin = utm.to_latlon(row['CO_O_X'],row['CO_O_Y'], 23, 'K')
+    dest = utm.to_latlon(row['CO_D_X'],row['CO_D_Y'], 23, 'K')
+    return geopy.distance.geodesic(origin, dest).meters
+
+data17_carros['DISTANCE'] = data17_carros.apply(lambda x: calculate_distance(x), axis=1)
+
+data_menor = data17_carros[data17_carros['DISTANCE'] <= 6000]
+data_menor = data_menor.head(4350)
+def calculate_distance_openservice(row):  
+    if row.name not in dict_routes.keys(): 
+        try:
+            origin = utm.to_latlon(row['CO_O_X'],row['CO_O_Y'], 23, 'K')
+            dest = utm.to_latlon(row['CO_D_X'],row['CO_D_Y'], 23, 'K')
+
+            coords = ((origin[1],origin[0]), (dest[1], dest[0]))
+            print(coords)
+            client = openrouteservice.Client(key='5b3ce3597851110001cf62484bb60b7582ea443aa2e89bb73cb2c985') # Specify your personal API key
+            routes = client.directions(coords)
+            geometry = routes['routes'][0]['geometry']
+            elevs = client.elevation_line('encodedpolyline', geometry)
+            lista = elevs['geometry']['coordinates']
+            dist = routes['routes'][0]['segments'][0]['distance']
+            duration = routes['routes'][0]['segments'][0]['duration']
+            return (lista, dist, duration)
+
+        except:
+            print("An exception occurred")
+            return ''
+
+data_menor['DISTANCE_LIST'] = data_menor.apply(lambda x: calculate_distance_openservice(x), axis=1)
+data_menor = data_menor.dropna(subset=['DISTANCE_LIST'])
+
+frame = data_menor[['DISTANCE_LIST']]
+print(frame)
+teste = teste.append(frame)
+print(teste)
+
+teste.to_csv('indices_public.csv')
+
+
---
+++ src/map.py
@@ -0,0 +1,156 @@
+import folium as folium
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+import unidecode
+import math
+import utm
+from shapely.geometry import shape, LineString, Polygon
+import matplotlib as mpl
+
+
+folder_images_maps = "../images/"
+def calculate_weighted_mean(data):
+    data['FE_VIA'] = data['FE_VIA'].apply(lambda x: 1 if math.isnan(x) else x)
+    data['FE_VIA'] = data['FE_VIA'].apply(lambda x: 1 if int(x) == 0 else x)
+    data['MP'] = data['FE_VIA'] * data['DURACAO']
+    data['MP_DIST'] = data['FE_VIA'] * data['DISTANCE']
+    return data
+
+mapa = gpd.GeoDataFrame.from_file("../data/shapes/Distritos_2017_region.shp", encoding='latin-1')
+mapa = mapa.to_crs({"init": "epsg:4326"})
+mapa['NomeDistri'] = mapa['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
+
+mapa = mapa[mapa['NumeroDist'] <=96]
+
+zonas = gpd.GeoDataFrame.from_file("../data/shapes/Zonas_2017_region.shp", encoding='latin-1')
+zonas = zonas.to_crs({"init": "epsg:4326"}) 
+zonas['NomeDistri'] = zonas['NomeDistri'].apply(lambda x: unidecode.unidecode(x))
+zonas['NomeZona'] = zonas['NomeZona'].apply(lambda x: unidecode.unidecode(x))
+
+zonas_teste = zonas[zonas['NumeroMuni'] == 36]
+
+folder_data = "../data/"
+arq17 = "dados17.csv"
+
+data17 = pd.read_csv(folder_data + arq17, dtype={'ZONA_O': str, 'ZONA_D': str}, header=0,delimiter=";", low_memory=False) 
+data17 = data17.dropna(subset=['CO_O_X'])
+
+data17 = data17.drop(['ID_DOM', 'FE_DOM', 'VIA_BICI','TP_ESTBICI','F_FAM','FE_FAM','FAMILIA','NO_MORAF',
+                      'CONDMORA','QT_BANHO','QT_EMPRE','QT_AUTO','QT_MICRO','QT_LAVALOU','QT_GEL1'], axis=1)
+
+data17['DISTANCE'] = 0
+
+csv_file = folder_data + "regioes17.csv"
+mydict = []
+with open(csv_file, mode='r') as infile:
+    reader = csv.reader(infile, delimiter=";")
+    mydict = {rows[0]:rows[1] for rows in reader}
+
+csv_file = folder_data + "zonas17.csv"
+zonas_nomes = []
+with open(csv_file, mode='r') as infile:
+    reader = csv.reader(infile, delimiter=";")
+    zonas_nomes = {rows[0]:rows[1] for rows in reader}
+
+data17['NOME_O'] = data17['ZONA_O'].apply(lambda x: '' if pd.isnull(x) else mydict[x])
+data17['NOME_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else mydict[x])
+
+data17['ZONA_O'] = data17['ZONA_O'].apply(lambda x: '' if pd.isnull(x) else zonas_nomes[x])
+data17['ZONA_D'] = data17['ZONA_D'].apply(lambda x: '' if pd.isnull(x) else zonas_nomes[x])
+
+data17['NUM_TRANS'] = data17[['MODO1', 'MODO2','MODO3','MODO4']].count(axis=1)
+
+data17 = calculate_weighted_mean(data17)
+
+def load_districts(vehicle, orde):
+
+    data17_copy = data17
+
+    if vehicle != "0":
+        vehicles = vehicle.split(",")
+        vehicles_int = []
+        for v in vehicles:
+            vehicles_int.append(int(v))
+        data17_copy = data17_copy[data17_copy['MODOPRIN'].isin(vehicles_int)] 
+    
+    data_mp2 = data17_copy[[orde,  'MP']].groupby([orde]).sum().sort_values(by=['MP']).reset_index()
+    data_mp2 = data_mp2.set_index(orde)
+
+    data_mp_dist = data17_copy[[orde,  'MP_DIST']].groupby([orde]).sum().sort_values(by=['MP_DIST']).reset_index()
+    data_mp_dist = data_mp_dist.set_index(orde)
+
+    data_mp = data17_copy[[orde, 'MUNI_O', 'FE_VIA']].groupby([orde,'MUNI_O']).sum().sort_values(by=[orde]).reset_index()
+    data_mp = data_mp.set_index(orde)
+
+    data_renda = data17_copy[[orde, 'RENDA_FA']].groupby([orde]).mean().sort_values(by=['RENDA_FA']).reset_index()
+    data_renda = data_renda.set_index(orde)
+
+    data_trans = data17_copy[[orde, 'NUM_TRANS']].groupby([orde]).mean().sort_values(by=['NUM_TRANS']).reset_index()
+    data_trans = data_trans.set_index(orde)
+
+    df = mapa.set_index('NomeDistri').join(data_mp).join(data_mp2).join(data_renda).join(data_trans).join(data_mp_dist)
+    df['MEDIA'] = df['MP'] / df['FE_VIA']
+    df['MEDIA_DIST'] = df['MP_DIST'] / df['FE_VIA']
+    df = df.reset_index()
+
+    df = df.drop(['NumeroDist', 'Area_ha'], axis=1)
+
+    return df
+
+
+teste = load_districts("15", "NOME_D")
+
+
+cmap = mpl.cm.OrRd(np.linspace(0,1,20))
+cmap = mpl.colors.ListedColormap(cmap[10:,:-1])
+
+fig, ax = plt.subplots(1, 1)
+teste.plot(column='FE_VIA', ax=ax, legend=True, cmap=cmap)
+
+plt.axis('off')
+plt.savefig(folder_images_maps + 'quantidade_bike.png', bbox_inches='tight', pad_inches=0.0)
+
+
+
+
+
+
+
+flows = pd.read_csv("flows.csv", encoding='latin-1')
+lines = []
+viagens = []
+
+flows = flows.set_index('index')
+
+flows = flows.join(data17, lsuffix='_left', rsuffix='_right')
+
+distri = flows[['NOME_O', 'FE_VIA']].groupby(['NOME_O']).sum().sort_values(by=['FE_VIA']).reset_index()
+distri.columns = ['NOME_O', 'FE_VIA_CARRO']
+distri = distri.set_index('NOME_O')
+distri['FE_VIA_CARRO'] = distri['FE_VIA_CARRO'].astype(int)
+distri.columns = ['Viagens']
+
+distri = mapa.set_index('NomeDistri').join(distri)
+fig, ax = plt.subplots(1, 1)
+
+distri.plot(column='Viagens', ax=ax, legend=True, cmap=cmap)
+
+plt.axis('off')
+plt.savefig(folder_images_maps + 'quantidade_bike_potencial.png', bbox_inches='tight', pad_inches=0.0)
+
+teste = teste.fillna(0)
+distri = distri.fillna(0)
+
+
+teste = teste.set_index('index').join(distri[['Viagens']], lsuffix='_left', rsuffix='_right')
+teste['JOIN'] = teste['Viagens'] - teste['FE_VIA']
+print(teste)
+
+fig, ax = plt.subplots(1, 1)
+teste.plot(column='JOIN', ax=ax, legend=True, cmap=cmap)
+
+plt.axis('off')
+plt.savefig(folder_images_maps + 'quantidade_bike_join.png', bbox_inches='tight', pad_inches=0.0)
