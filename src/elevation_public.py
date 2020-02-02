5ce485760bad595e7f2009ce832b9b99b06601a4
Eduardo Santana <ezambomsantana@gmail.com>
2020-02-02 10:12:53 -0300

    public

--- src/elevation_public.py
+++ src/elevation_public.py
@@ -48,7 +48,7 @@ def calculate_distance(row):
 data17_carros['DISTANCE'] = data17_carros.apply(lambda x: calculate_distance(x), axis=1)
 
 data_menor = data17_carros[data17_carros['DISTANCE'] <= 6000]
-data_menor = data_menor.head(11350)
+data_menor = data_menor.head(15350)
 def calculate_distance_openservice(row):  
     if row.name not in dict_routes.keys(): 
         try:
@@ -57,7 +57,7 @@ def calculate_distance_openservice(row):
 
             coords = ((origin[1],origin[0]), (dest[1], dest[0]))
             print(coords)
-            client = openrouteservice.Client(key='5b3ce3597851110001cf6248758acbaf705a464388f196e85dab1a24') # Specify your personal API key
+            client = openrouteservice.Client(key='5b3ce3597851110001cf624887051db3a97b4cd28bfdb001dc78e025') # Specify your personal API key
             routes = client.directions(coords)
             geometry = routes['routes'][0]['geometry']
             elevs = client.elevation_line('encodedpolyline', geometry)
